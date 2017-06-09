c1ce3aa4ac89f88d7e2321780893d907089437e3
rubelio.cardona <rubelio.cardona@pragma.com.co>
2017-06-09 17:38:10 -0500

    cambio plugin ios

--- src/ios/smartconfigios.h
+++ src/ios/smartconfigios.h
@@ -1,13 +1,13 @@
 #import <Cordova/CDV.h>
 #import <UIKit/UIKit.h>
 #import "../SmartConfigLib/smartConfig.h"
-#import "../SmartConfigLib/DataTransform.h"
-#import "../SmartConfigLib/RemoteSession.h"
-#import "../SmartConfigLib/HttpManager.h"
-#import "../SmartConfigLib/DeviceViewController.h"
-#import "../SmartConfigLib/LCToast.h"
+//#import "../SmartConfigLib/DataTransform.h"
+//#import "../SmartConfigLib/RemoteSession.h"
+//#import "../SmartConfigLib/HttpManager.h"
+//#import "../SmartConfigLib/DeviceViewController.h"
+//#import "../SmartConfigLib/LCToast.h"
 #import "../SmartConfigLib/ConfigClass.h"
-#import "../SmartConfigLib/DeviceModel.h"
+//#import "../SmartConfigLib/DeviceModel.h"
 
 @interface smartconfig : CDVPlugin
 {
